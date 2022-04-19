f4faab7b959c9d2f8092cd57fc849c784938dbf3
SerhiiBuchyn <serhii.buchyn@gmail.com>
2022-04-19 20:47:57 +0300

    add my mini project

--- Mini Project JS/post-detailsUser.js
+++ Mini Project JS/post-detailsUser.js
@@ -2,7 +2,8 @@
 //         "userId": 1,
 //         "id": 1,
 //         "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-//         "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+//         "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae
+//         ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
 //     },
 
 const postDetailsKey = 'postDetails';
@@ -24,7 +25,8 @@ post.innerHTML = `
 //     "id": 1,
 //     "name": "id labore ex et quam laborum",
 //     "email": "Eliseo@gardner.biz",
-//     "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+//     "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor
+//     quam autem quasi\nreiciendis et nam sapiente accusantium"
 // },
 
 const allCommentsWrap = document.createElement('div');
